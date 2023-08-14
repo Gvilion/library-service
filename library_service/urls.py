a252a0bdeaf72e4944731080a739a4ebbebe4467
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-14 18:33:29 +0300

    Added payment url in app urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls", namespace="books"))
+    path("api/", include("books.urls", namespace="books")),
+    path("api/", include("payment.urls", namespace="payment")),
+
 ]
