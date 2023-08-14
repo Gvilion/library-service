0c939d87bf41ec089f79b8fa8313b9c1753f905e
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-14 19:15:42 +0300

    Added payment url to library_service

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/books/", include("books.urls", namespace="books")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/", include("books.urls", namespace="books")),
+
+    path("api/", include("payment.urls", namespace="payment")),
+
 ]
