a9d22c23473c1c8af8d7c7afbed2ca6cb98646a9
YOUR_NAME <YOUR_EMAIL>
2023-08-14 15:11:32 +0300

    removed groups and user_permissions

--- user/models.py
+++ user/models.py
@@ -49,23 +49,3 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
-
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name=_('groups'),
-        blank=True,
-        related_name='custom_user_set',
-        help_text=_(
-            'The groups this user belongs to. A user will get all permissions '
-            'granted to each of their groups.'
-        ),
-        related_query_name='user',
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name=_('user permissions'),
-        blank=True,
-        related_name='custom_user_set',
-        help_text=_('Specific permissions for this user.'),
-        related_query_name='user',
-    )
