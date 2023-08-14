0c65dca2095f73408d76b7bfa212c23841c0d168
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-14 20:09:22 +0300

    fixed date validation and property in models.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 from django.db import models
 from django.core.exceptions import ValidationError
@@ -9,23 +11,22 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings")
 
     @property
     def is_active(self):
-        return self.actual_return_date in None
+        return self.actual_return_date is None
 
     @staticmethod
-    def validate_date(expected_return_date, borrow_date, error_to_raise):
-        if expected_return_date < borrow_date:
+    def validate_date(expected_return_date, error_to_raise):
+        if expected_return_date < datetime.date.today():
             raise error_to_raise(
                 "Expected return date cannot be earlier than borrow date."
             )
 
     def clean(self):
         Borrowing.validate_date(self.expected_return_date,
-                                self.borrow_date,
                                 ValidationError)
 
     def save(self,
