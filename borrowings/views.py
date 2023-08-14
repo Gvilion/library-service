92327e7943fed275f1cdb3619a65dd47b7f83cbb
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-14 20:12:10 +0300

    added borrowings viewset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingDetailSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
+
+    def get_queryset(self):
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+        queryset = Borrowing.objects.all()
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        if user_id:
+            if self.request.user.is_staff:
+                queryset = queryset.filter(user_id=user_id)
+
+        if is_active is not None:
+            if is_active in ["false", "False", 0]:
+                is_active = False
+            else:
+                is_active = True
+            queryset = queryset.filter(actual_return_date__isnull=bool(is_active))
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
