c7bd7bb8a61df6811ff7c76fadbaabd829ab365f
YOUR_NAME <YOUR_EMAIL>
2023-08-14 16:51:09 +0300

    added path to user app urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/books", include("books.urls", namespace="books"))
+    path("api/books", include("books.urls", namespace="books")),
+
+    path("api/user/", include("user.urls", namespace="user")),
 ]
