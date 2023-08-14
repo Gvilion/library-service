8080d54da5e3497c7fc795b6e9c18e6c8b8e7c10
Vlad <vladislav.zhyla@gmail.com>
2023-08-14 15:37:37 +0300

    Endpoints fixed

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/books", include("books.urls", namespace="books"))
+    path("api/", include("books.urls", namespace="books"))
 ]
