e01797b5e89dfd6f8ca549b820a3a1c0f57fbb89
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-14 17:15:18 +0300

    added expected_return_date validation

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from books.models import Book
 
@@ -9,16 +10,36 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-
-    # uncomment this when custom User model is implemented:
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     @property
     def is_active(self):
         return self.actual_return_date in None
 
+    @staticmethod
+    def validate_date(expected_return_date, borrow_date, error_to_raise):
+        if expected_return_date < borrow_date:
+            raise error_to_raise(
+                "Expected return date cannot be earlier than borrow date."
+            )
+
+    def clean(self):
+        Borrowing.validate_date(self.expected_return_date,
+                                self.borrow_date,
+                                ValidationError)
+
+    def save(self,
+             force_insert=False,
+             force_update=False,
+             using=None,
+             update_fields=None):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
     class Meta:
         ordering = ["expected_return_date"]
 
     def __str__(self) -> str:
-        return str(self.actual_return_date)
+        return str(self.borrow_date)
