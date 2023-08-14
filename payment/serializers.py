0309a0f21c029113655af618942fdbd2d716b965
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-14 22:51:26 +0300

    Separated PaymentListDetailSerializer on List and Detail Payment Serializers

--- payment/serializers.py
+++ payment/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from borrowings.serializers import BorrowingListSerializer
 from payment.models import Payment
 
 
@@ -18,9 +19,32 @@ class PaymentSerializer(serializers.ModelSerializer):
         )
 
 
-class PaymentListDetailSerializer(PaymentSerializer):
+class PaymentListSerializer(PaymentSerializer):
+    user = serializers.CharField(
+        source="user.email",
+        read_only=True
+    )
+    borrowing = serializers.CharField(
+        source="borrowing.book.title",
+        read_only=True
+    )
+
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "user",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        )
+
+class PaymentDetailSerializer(PaymentSerializer):
     user = serializers.CharField(source="user.email")
-    borrowing = serializers.CharField(source="borrowing.book.title")
+    borrowing = BorrowingListSerializer(read_only=True, many=False)
 
     class Meta:
         model = Payment
