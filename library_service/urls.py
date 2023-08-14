1959fdd5a8647275a44bb1cfed79e8d079dc8f4e
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-14 19:42:38 +0300

    Added urls from develop branch

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls", namespace="books")),
-
+    path("api/books/", include("books.urls", namespace="books")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/", include("payment.urls", namespace="payment")),
 
 ]
