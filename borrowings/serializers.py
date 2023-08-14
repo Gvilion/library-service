241c6e522177efb63b2427d6eccce36abca62443
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-14 23:41:07 +0300

    added separate borrowing create serializers.py with basic logic and validation

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -55,3 +55,34 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "is_active",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "borrow_date", "expected_return_date", "actual_return_date")
+
+    def validate(self, attrs):
+        book = attrs.get("book")
+        expected_return_date = attrs.get("expected_return_date")
+
+        if book.inventory == 0:
+            raise serializers.ValidationError(f"There's no more '{book.title}' books!")
+
+        Borrowing.validate_date(
+            expected_return_date,
+            ValidationError,
+        )
+        return attrs
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        borrowing = Borrowing.objects.create(
+            book=book,
+            expected_return_date=validated_data["expected_return_date"],
+            actual_return_date=validated_data["actual_return_date"],
+            user=self.context["request"].user,
+        )
+        book.inventory -= 1
+        book.save()
+        return borrowing
