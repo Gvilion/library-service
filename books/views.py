6224fa1d499c000911d5cc4ceb5b3305001ecf45
Vlad <vladislav.zhyla@gmail.com>
2023-08-14 15:12:47 +0300

    Added views for books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.permissions import IsAdminOrIfAuthenticatedReadOnly
+from books.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
