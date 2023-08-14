93339c69ca6c88e07a5f90b21cfe421d595b4bf1
Vlad <vladislav.zhyla@gmail.com>
2023-08-14 15:19:02 +0300

    Title and author unique together now

--- books/models.py
+++ books/models.py
@@ -7,11 +7,14 @@ class Book(models.Model):
         ("SOFT", "Soft")
     )
 
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=4, choices=CHOICES)
     inventory = models.PositiveIntegerField(default=1)
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ("title", "author",)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
