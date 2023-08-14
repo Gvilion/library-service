b31a9970dc18039c7d2820d21a11e285674d9304
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-14 18:37:27 +0300

    Added PaymentViewSet

---
+++ payment/views.py
@@ -0,0 +1,45 @@
+from rest_framework import mixins, viewsets
+
+from payment.models import Payment
+from payment.permissions import IsAdminOrIfAuthenticatedReadOnly
+from payment.serializers import (
+    PaymentSerializer,
+    PaymentListDetailSerializer
+)
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly, ]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            queryset = Payment.objects.select_related("user")
+
+            if not self.request.user.is_staff:
+                queryset = queryset.filter(user=self.request.user)
+
+        if self.action == "retrieve":
+            queryset = Payment.objects.select_related("user")
+
+        return queryset
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+
+        if self.action == "list":
+            serializer_class = PaymentListDetailSerializer
+        elif self.action == "retrieve":
+            serializer_class = PaymentListDetailSerializer
+
+        return serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
