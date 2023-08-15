1a21627e9140cbc384c5acd9eb0b2c055ffa3ec3
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-15 23:05:00 +0300

    Fixed router register name for better view url

--- payment/urls.py
+++ payment/urls.py
@@ -8,7 +8,7 @@ from payment.views import (
 )
 
 router = routers.DefaultRouter()
-router.register("payments", PaymentViewSet)
+router.register("", PaymentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
