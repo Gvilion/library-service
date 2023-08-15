661ca0acb086aaef901806623afdcd59d8144f84
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-15 11:21:23 +0300

    Update documentation URLs

--- library_service/urls.py
+++ library_service/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "api/schema/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui"
     ),
