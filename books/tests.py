0ef58362ddde8125e27837fd9e153d6a555e15e8
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-15 20:47:43 +0300

    Add tests for book model

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class BookModelTestCase(TestCase):
+
+    def setUp(self):
+        Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            cover="HARD",
+            inventory=10,
+            daily_fee=12.99
+        )
+
+    def test_book_creation(self):
+        book = Book.objects.get(title="Test Book")
+        self.assertEqual(book.author, "Test Author")
+        self.assertEqual(book.cover, "HARD")
+        self.assertEqual(book.inventory, 10)
+        self.assertEqual(book.daily_fee, 12.99)
+
+    def test_book_str_method(self):
+        book = Book.objects.get(title="Test Book")
+        self.assertEqual(str(book), "Test Book by Test Author")
+
+    def test_unique_together_constraint(self):
+        with self.assertRaises(Exception):
+            Book.objects.create(
+                title="Test Book",
+                author="Test Author",
+                cover="SOFT",
+                inventory=5,
+                daily_fee=9.99
+            )
