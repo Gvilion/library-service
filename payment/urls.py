1377b45cc402b847539915d308d75925c7622c70
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-15 16:32:14 +0300

    Added the path for success and cancel urls

--- payment/urls.py
+++ payment/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payment.views import PaymentViewSet
+from payment.views import (
+    PaymentViewSet,
+    SuccessPaymentView,
+    CancelPaymentView
+)
 
 router = routers.DefaultRouter()
 router.register("payments", PaymentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "<int:pk>/success/",
+        SuccessPaymentView.as_view(),
+        name='payment-success'
+    ),
+    path(
+        "cancel/",
+        CancelPaymentView.as_view(),
+        name="payment-cancel"
+    )
 ]
 
 app_name = "payment"
