bab0d775f69c877289564aa755a01dfe55753e06
YOUR_NAME <YOUR_EMAIL>
2023-08-16 13:12:13 +0300

    added send_message_borrowing_return_with_payment_success if status is PAID after row in DB

--- borrowings/signals.py
+++ borrowings/signals.py
@@ -2,7 +2,11 @@ from dispatch import receiver
 from django.db.models.signals import post_save
 from django.dispatch import Signal
 from .models import Borrowing
-from .telegram_bot import send_message_of_borrowing_creation, send_message_of_borrowing_return
+from payment.models import Payment
+from .telegram_bot import (
+    send_message_of_borrowing_creation,
+    send_message_borrowing_return_with_payment_success
+)
 
 new_borrowing_created = Signal()
 
@@ -13,7 +17,7 @@ def send_new_borrowing_notification(instance, created, **kwargs):
         send_message_of_borrowing_creation(instance)
 
 
-@receiver(post_save, sender=Borrowing)
+@receiver(post_save, sender=Payment)
 def send_returned_borrowing_notification(instance, created, **kwargs):
-    if not created and instance.actual_return_date:
-        send_message_of_borrowing_return(instance)
+    if instance.status == "PAID":
+        send_message_borrowing_return_with_payment_success(instance)
