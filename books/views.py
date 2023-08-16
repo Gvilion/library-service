490f1faf1569e373e06f5880319b38e1845dae8c
YOUR_NAME <YOUR_EMAIL>
2023-08-16 18:00:59 +0300

    added filters for author and title

--- books/views.py
+++ books/views.py
@@ -9,3 +9,17 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset.distinct()
