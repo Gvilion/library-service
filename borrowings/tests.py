a62f3b3646e5d5451e3d5cc58f0a82bb35ba720a
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-16 11:57:09 +0300

    Add tests for Borrowing model

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from books.models import Book
+from borrowings.models import Borrowing
+from datetime import date, timedelta
 
-# Create your tests here.
+
+class BorrowingModelTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email="user@email.com", password="TestPassword123"
+        )
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            cover="HARD",
+            inventory=5,
+            daily_fee=12.99
+        )
+        self.borrowing_data = {
+            "expected_return_date": date.today() + timedelta(days=7),
+            "book": self.book,
+            "user": self.user,
+        }
+
+    def test_borrowing_creation(self):
+        borrowing = Borrowing.objects.create(**self.borrowing_data)
+        self.assertEqual(Borrowing.objects.count(), 1)
+        self.assertEqual(
+            borrowing.expected_return_date,
+            self.borrowing_data["expected_return_date"]
+        )
+        self.assertEqual(borrowing.book, self.book)
+        self.assertEqual(borrowing.user, self.user)
+        self.assertIsNone(borrowing.actual_return_date)
+        self.assertTrue(borrowing.is_active)
+
+    def test_borrowing_validation_invalid_expected_return_date(self):
+        invalid_data = self.borrowing_data.copy()
+        invalid_data["expected_return_date"] = date.today() - timedelta(days=1)
+        with self.assertRaises(ValidationError):
+            Borrowing.objects.create(**invalid_data)
+
+    def test_borrowing_return(self):
+        borrowing = Borrowing.objects.create(**self.borrowing_data)
+        borrowing.actual_return_date = date.today()
+        borrowing.save()
+        self.assertFalse(borrowing.is_active)
+
+    def test_borrowing_clean_method(self):
+        borrowing = Borrowing(**self.borrowing_data)
+        borrowing.clean()
+        borrowing.expected_return_date = date.today() - timedelta(days=1)
+        with self.assertRaises(ValidationError):
+            borrowing.clean()
+
+    def test_borrowing_str_representation(self):
+        borrowing = Borrowing(**self.borrowing_data)
+        self.assertEqual(str(borrowing), str(borrowing.borrow_date))
